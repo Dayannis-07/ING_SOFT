6a510bfb621126e3eda7a16bd4b7516eacda440e
jsusm <jesusjaviermarcanomora.2005@gmail.com>
2025-03-05 01:40:44 -0400

    Mostrar eventos en la vista de calendario

--- src/main/views/CalendarView.java
+++ src/main/views/CalendarView.java
@@ -1,11 +1,15 @@
 package main.views;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import main.components.FooterFactory;
 import main.components.HeaderFactory;
+import main.controllers.CalendarController;
+import main.utils.Palette;
 
 public class CalendarView {
     private JFrame frame;
@@ -16,8 +20,10 @@ public class CalendarView {
     private JLabel nextMonthLabel;
     private JLabel prevMonthLabel;
     Calendar cal;
+    CalendarController controller;
 
     public CalendarView() {
+        controller = new CalendarController(true);
         initializeFrame();
         initializeHeaderAndFooter();
         initializeCalendarPanel();
@@ -38,17 +44,18 @@ public class CalendarView {
         frame.setLocationRelativeTo(null);
     }
 
-    private void initializeHeaderAndFooter() { 
-        // Añadir el header y footer usando HeaderFactory y FooterFactory 
+    private void initializeHeaderAndFooter() {
+        // Añadir el header y footer usando HeaderFactory y FooterFactory
         JPanel header = new HeaderFactory(frame);
-        JPanel footer = new FooterFactory(frame); 
-        frame.add(header, BorderLayout.NORTH); 
-        frame.add(footer, BorderLayout.SOUTH); 
+        JPanel footer = new FooterFactory(frame);
+        frame.add(header, BorderLayout.NORTH);
+        frame.add(footer, BorderLayout.SOUTH);
     }
 
     private void initializeCalendarPanel() {
         calendarPanel = new JPanel(new BorderLayout());
         frame.add(calendarPanel, BorderLayout.CENTER);
+        calendarPanel.setBackground(Palette.instance().getWhite());
         frame.addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent componentEvent) {
                 calendarPanel.setBounds(10, 100, frame.getWidth() - 40, frame.getHeight() - 150);
@@ -77,6 +84,7 @@ public class CalendarView {
 
     private JPanel createMonthPanel() {
         JPanel monthPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        monthPanel.setBackground(Palette.instance().getWhite());
         monthLabel = createMonthLabel();
         monthPanel.add(monthLabel);
         createMonthButtons(monthPanel);
@@ -92,38 +100,39 @@ public class CalendarView {
     private void createMonthButtons(JPanel monthPanel) {
         ImageIcon nextIcon = new ImageIcon(HeaderFactory.class.getResource("/assets/right_arrow.png"));
         ImageIcon prevIcon = new ImageIcon(HeaderFactory.class.getResource("/assets/left_arrow.png"));
-    
+
         Image nextImage = nextIcon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
         Image prevImage = prevIcon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-    
+
         nextIcon = new ImageIcon(nextImage);
         prevIcon = new ImageIcon(prevImage);
-    
+
         nextMonthLabel = new JLabel(nextIcon);
         nextMonthLabel.setBounds(200, 60, 50, 50);
         prevMonthLabel = new JLabel(prevIcon);
         prevMonthLabel.setBounds(20, 60, 50, 50);
-    
+
         monthPanel.add(prevMonthLabel);
         monthPanel.add(monthLabel);
         monthPanel.add(nextMonthLabel);
-    
+
         nextMonthLabel.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 nextMonth();
             }
         });
-    
+
         prevMonthLabel.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 prevMonth();
             }
         });
     }
-    
+
     private JPanel createDaysPanel() {
         JPanel daysPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
+        daysPanel.setBackground(Palette.instance().getWhite());
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
@@ -133,7 +142,7 @@ public class CalendarView {
     }
 
     private void addDayLabels(JPanel daysPanel, GridBagConstraints gbc) {
-        String[] days = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+        String[] days = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
         for (int i = 0; i < days.length; i++) {
             JLabel dayLabel = new JLabel(days[i], SwingConstants.CENTER);
             dayLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
@@ -150,6 +159,7 @@ public class CalendarView {
         dayPanels = new JPanel[42];
         for (int i = 0; i < 42; i++) {
             dayPanels[i] = new JPanel();
+            dayPanels[i].setBackground(Palette.instance().getWhite());
             dayPanels[i].setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
             dayPanels[i].setLayout(new BorderLayout());
             JLabel dayLabel = new JLabel("", SwingConstants.RIGHT);
@@ -184,13 +194,13 @@ public class CalendarView {
 
     private void cleanDaysPanels() {
         for (int i = 0; i < 42; i++) {
-            JLabel label = (JLabel) dayPanels[i].getComponent(0);
-            label.setText("");
+            dayPanels[i].removeAll();
         }
     }
 
     private void updateCalendar() {
-        String[] months = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        String[] months = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+                "Octubre", "Noviembre", "Diciembre" };
         monthLabel.setText(months[month] + " " + year);
 
         Calendar cal = new GregorianCalendar(year, month, 1);
@@ -199,13 +209,36 @@ public class CalendarView {
             startDay += 7;
         }
 
-        int daysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-
         cleanDaysPanels();
 
+        int daysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
         for (int i = 0; i < daysInMonth; i++) {
-            JLabel label = (JLabel) dayPanels[i + startDay].getComponent(0);
-            label.setText(String.valueOf(i + 1));
+            JLabel label = new JLabel(String.valueOf(i + 1), SwingConstants.RIGHT);
+
+            var posts = controller.getPostsByDate(i + 1, month + 1);
+
+            var postsPanel = new JPanel();
+            postsPanel.setBackground(Palette.instance().getWhite());
+            postsPanel.setLayout(new BoxLayout(postsPanel, BoxLayout.Y_AXIS));
+
+            for (int j = 0; j < posts.size(); j++) {
+                JLabel eventLbl = new JLabel(posts.get(j).getTitle());
+
+                final int index = j;
+                eventLbl.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        frame.dispose();
+                        new PostView(posts.get(index).getId());
+                    }
+                });
+                postsPanel.add(eventLbl);
+                postsPanel.add(Box.createRigidArea(new Dimension(5, 10)));
+            }
+
+            dayPanels[i + startDay].add(label, BorderLayout.NORTH);
+            dayPanels[i + startDay].add(postsPanel, BorderLayout.CENTER);
         }
     }
 
@@ -213,4 +246,3 @@ public class CalendarView {
         new CalendarView();
     }
 }
-
